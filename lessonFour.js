f3e25b665b7763e157407fd353bac712851cefbf
tunny17 <tvnji01@gmail.com>
2025-06-10 12:10:56 +0100

    feat: practice questions

--- lessonFour.js
+++ lessonFour.js
@@ -1,21 +1,36 @@
-const fs = require('fs');
+// const fs = require('fs');
 
-const output = fs
-  .readFileSync('data.txt', 'utf8')
-  .trim()
-  .split('\n')
-  .map((line) => line.split(' '))
-  .reduce((customers, line) => {
-    customers[line[0]] = customers[line[0]] || [];
-    customers[line[0]].push({
-      name: line[1],
-      price: line[2],
-      quantity: line[3]
-    });
-    return customers;
-  }, {});
+// const output = fs
+//   .readFileSync('data.txt', 'utf8')
+//   .trim()
+//   .split('\n')
+//   .map((line) => line.split(' '))
+//   .reduce((customers, line) => {
+//     customers[line[0]] = customers[line[0]] || [];
+//     customers[line[0]].push({
+//       name: line[1],
+//       price: line[2],
+//       quantity: line[3]
+//     });
+//     return customers;
+//   }, {});
 
-console.log(JSON.stringify(output, null, 2));
+// console.log(JSON.stringify(output, null, 2));
 // note:
 // trim() removes line breaks or spaces
 // split('\n') splits the string into an array of lines based on the line breaks
+
+// practice code:
+const people = [
+  { name: 'Alice', age: 21 },
+  { name: 'Bob', age: 25 },
+  { name: 'Charlie', age: 21 }
+];
+
+const grouped = people.reduce((acc, person) => {
+  acc[person.age] = acc[person.age] || [];
+  acc[person.age].push(person.name);
+  return acc;
+}, {});
+
+console.log(grouped);
--- lessonOne.js
+++ lessonOne.js
@@ -1,18 +1,25 @@
-const animals = [
-  {name: 'Fluffykins', species: 'rabbit'},
-  {name: 'Caro', species: 'dog'},
-  {name: 'Hamilton', species: 'dog'},
-  {name: 'Harold', species: 'fish'},
-  {name: 'Ursla', species: 'cat'},
-  {name: 'Jimmy', species: 'fish'},
-]
+// const animals = [
+//   { name: 'Fluffykins', species: 'rabbit' },
+//   { name: 'Caro', species: 'dog' },
+//   { name: 'Hamilton', species: 'dog' },
+//   { name: 'Harold', species: 'fish' },
+//   { name: 'Ursla', species: 'cat' },
+//   { name: 'Jimmy', species: 'fish' }
+// ];
 
-const isDog = (animal) => {
-  return animal.species === 'dog';
-}
+// const isDog = (animal) => {
+//   return animal.species === 'dog';
+// };
 
-const dogs = animals.filter(isDog);
-const otherAnimals = animals.reject(isDog);
+// const dogs = animals.filter(isDog);
 
-console.log(dogs);
+// console.log(dogs);
 // console.log(otherAnimals);
+
+const people = [
+  { name: 'Alice', age: 17 },
+  { name: 'Bob', age: 22 },
+  { name: 'Charlie', age: 16 }
+];
+const adults = people.reject((person) => person.age >= 18);
+console.log(adults);
